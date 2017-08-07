af51aac21d77d4e54a2ddbf9222cf9f0b06fb994
arsenaler <youhaohaoxuexi@126.com>
2017-08-07 13:40:22 +0800

    add function to add/del/edit nic

--- vcenter.py
+++ vcenter.py
@@ -4,6 +4,8 @@
 import os
 import os.path
 import time
+from pysphere import *
+from pysphere.resources import VimService_services as VI
 from pysphere import VIServer
 from pysphere import VIException, VIApiException, FaultTypes
 import logging
@@ -140,3 +142,147 @@ class vcenter(object):
         return vm.get_property('ip_address', from_cache=False)
 
 
+    @classmethod
+    def add_nic(cls, vm, label, nic_type):
+
+        vm_obj = cls.server.get_vm_by_name(vm)
+        if not vm_obj:
+            raise Exception("VM %s not found" % vm)
+
+        #Invoke ReconfigVM_Task
+        request = VI.ReconfigVM_TaskRequestMsg()
+        _this = request.new__this(vm_obj._mor)
+        _this.set_attribute_type(vm_obj._mor.get_attribute_type())
+        request.set_element__this(_this)
+        spec = request.new_spec()
+        #add a NIC. the network Name must be set as the device name.
+        dev_change = spec.new_deviceChange()
+        dev_change.set_element_operation("add")
+        if nic_type == 'VMXNET3':
+            nic_ctlr = VI.ns0.VirtualVmxnet3_Def("nic_ctlr").pyclass()
+        elif nic_type == 'E1000':
+            nic_ctlr = VI.ns0.VirtualE1000_Def("nic_ctlr").pyclass()
+        else:
+            nic_ctlr = VI.ns0.VirtualVmxnet2_Def("nic_ctlr").pyclass()
+
+        nic_backing = VI.ns0.VirtualEthernetCardNetworkBackingInfo_Def("nic_backing").pyclass()
+        nic_backing.set_element_deviceName(label)
+        nic_ctlr.set_element_addressType("generated")
+        nic_ctlr.set_element_backing(nic_backing)
+        nic_ctlr.set_element_key(4)
+        dev_change.set_element_device(nic_ctlr)
+
+        spec.set_element_deviceChange([dev_change])
+        request.set_element_spec(spec)
+        ret = cls.server._proxy.ReconfigVM_Task(request)._returnval
+
+        #Wait for the task to finish
+        task = VITask(ret, cls.server)
+        status = task.wait_for_state([task.STATE_SUCCESS, task.STATE_ERROR])
+        if status == task.STATE_SUCCESS:
+            print "VM %s successfully reconfigured" % vm
+
+        elif status == task.STATE_ERROR:
+            print "Error reconfiguring vm: %s" % vm, task.get_error_message()
+
+    @classmethod
+    def delete_nic(cls, vm, label):
+
+        vm_obj = cls.server.get_vm_by_name(vm)
+        if not vm_obj:
+            raise Exception("VM %s not found" % vm)
+
+        #Find Virtual Nic device
+        net_device = None
+        print vm_obj.properties.config.hardware.device
+        for dev in vm_obj.properties.config.hardware.device:
+            if dev._type in ["VirtualE1000", "VirtualVmxnet3", "VirtualVmxnet2"]   \
+                    and dev.backing.deviceName == label:
+
+                net_device = dev._obj
+            if not net_device:
+                raise Exception("The vm seems to lack a Virtual Nic")
+
+        print net_device
+
+        #Invoke ReconfigVM_Task
+        request = VI.ReconfigVM_TaskRequestMsg()
+        _this = request.new__this(vm_obj._mor)
+        _this.set_attribute_type(vm_obj._mor.get_attribute_type())
+        request.set_element__this(_this)
+        spec = request.new_spec()
+
+        dev_change = spec.new_deviceChange()
+        dev_change.set_element_device(net_device)
+        dev_change.set_element_operation("remove")
+        spec.set_element_deviceChange([dev_change])
+        print spec
+        request.set_element_spec(spec)
+
+        spec.set_element_deviceChange([dev_change])
+        request.set_element_spec(spec)
+        ret = cls.server._proxy.ReconfigVM_Task(request)._returnval
+
+        #Wait for the task to finish
+        task = VITask(ret, cls.server)
+        status = task.wait_for_state([task.STATE_SUCCESS, task.STATE_ERROR])
+        if status == task.STATE_SUCCESS:
+            print "VM %s successfully reconfigured" % vm
+
+        elif status == task.STATE_ERROR:
+            print "Error reconfiguring vm: %s" % vm, task.get_error_message()
+
+    @classmethod
+    def change_nic_vlan(cls, vm, src_vlan, dst_vlan):
+
+        vm_obj = cls.server.get_vm_by_name(vm)
+        if not vm_obj:
+            raise Exception("VM %s not found" % vm)
+
+    #Find Virtual Nic device
+        net_device = None
+        for dev in vm_obj.properties.config.hardware.device:
+            if dev._type in ["VirtualE1000", "VirtualVmxnet3", "VirtualVmxnet2"]   \
+                    and dev.backing.deviceName == src_vlan:
+
+                net_device = dev._obj
+            if not net_device:
+                raise Exception("The vm seems to lack a Virtual Nic")
+        net_device.Backing.set_element_deviceName(dst_vlan)
+
+    #Invoke ReconfigVM_Task
+        request = VI.ReconfigVM_TaskRequestMsg()
+        _this = request.new__this(vm_obj._mor)
+        _this.set_attribute_type(vm_obj._mor.get_attribute_type())
+        request.set_element__this(_this)
+        spec = request.new_spec()
+        #add a NIC. the network Name must be set as the device name.
+        dev_change = spec.new_deviceChange()
+        dev_change.set_element_device(net_device)
+        dev_change.set_element_operation("edit")
+        spec.set_element_deviceChange([dev_change])
+        request.set_element_spec(spec)
+        ret = cls.server._proxy.ReconfigVM_Task(request)._returnval
+
+        #Wait for the task to finish
+        task = VITask(ret, cls.server)
+        status = task.wait_for_state([task.STATE_SUCCESS, task.STATE_ERROR])
+        if status == task.STATE_SUCCESS:
+            print "VM %s successfully reconfigured" % vm
+
+        elif status == task.STATE_ERROR:
+            print "Error reconfiguring vm: %s" % vm, task.get_error_message()
+
+
+
+vcenter.connect_server('10.8.2.21', 'root', 'sonicpassword')
+print vcenter.get_vm('wdong-remote_vm')
+vms = vcenter.get_all_vms()
+
+
+for vm in vms:
+    if 'wdong' in vm:
+        print vm
+
+print '=================================='
+print '#####################################'
